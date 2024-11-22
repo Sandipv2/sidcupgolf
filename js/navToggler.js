5b654ecd78aebb2b2f27ed23227ff5bfecbc808f
sandipv2 <sandiipvarma@gmail.com>
2024-11-22 22:35:58 +0530

    Removed nav btns

--- js/navToggler.js
+++ js/navToggler.js
@@ -1,23 +1,32 @@
 export const navToggler = () => {
     const toggler = document.querySelector("nav .nav-toggler");
     const ul = document.querySelector("nav .nav-menu-container ul");
-    const headerBtn = document.querySelector("nav .header-buttons");
+    // const headerBtn = document.querySelector("nav .header-buttons");
 
     let isNavHidden = true;
 
     toggler.addEventListener("click", () => {
         if (isNavHidden) {
             ul.style.display = "flex";
-            headerBtn.style.display = "flex";
+            //headerBtn.style.display = "flex";
             isNavHidden = false;
         } else {
             ul.style.display = "none";
-            headerBtn.style.display = "none";
+            //headerBtn.style.display = "none";
             isNavHidden = true;
 
         }
     })
 
+    window.addEventListener("resize", () => {
+        if(window.innerWidth > 575) {
+            ul.style.display = "flex"
+        }
+        else {
+            ul.style.display = "none"
+            
+        }
+    })
 }
 
 navToggler();
